51025d700fc0d0c2deec81d10d10c628db8ee4b9
Matthew Vaughan <mv@mind.contact>
2022-08-18 16:48:22 -0400

    update Database classes

--- Database.php
+++ Database.php
@@ -1,37 +1,23 @@
 <?php
 require_once 'Singleton.php';
 
-interface Database {
-    public function connect();
-    public function close();
+interface Transactions {
     public function select($table, $values);
     public function update($table, $map);
     public function insert($table, $map);
 }
 
-class AtomicDatabase implements Database {
-    use Singleton;
+class Database implements Transactions {
     
-    private $username;
-    private $password;
-    private $database;
-
-    private $db;
-
-    protected function __contruct() {
-        $this->username = getenv("DB_USERNAME");
-        $this->password = getenv("DB_PASSWORD");
-        $this->database = getenv("DB_DATABASE");
-    }
-
+    protected $db;
     public function connect() {
-        $this->db = new mysqli("localhost",$this->username,$this->password,$this->database);
+        $this->db = new mysqli("127.0.0.1",getenv("DB_USERNAME"),getenv("DB_PASSWORD"),genenv("DB_DATABASE"));
     }
 
     public function close() {
         $this->db->close();
     }
-    
+
     public function keyValEq($map) {
         $expr = "";
         foreach ($map as $key => $value) {
@@ -42,35 +28,26 @@ class AtomicDatabase implements Database {
     }
 
     public function select($table, $values, $where="") {
-        $this->connect();
-
         $vals = "";
         for( $i=0; $i < sizeof($values); $i++ ) {
             if ( $vals != "" ) $vals = $vals . ",";
             $vals = $vals . "'".$values[$i]."'";
         }
 
-        $result = null;
         if ( $where != "" ) {
             $expr = $this->keyValEq($where);
-            $result = $this->db->query("SELECT {$vals} FROM '{$table}' WHERE {$expr}");
+            return $this->db->query("SELECT {$vals} FROM '{$table}' WHERE {$expr}");
         } else {
-            $result = $this->db->query("SELECT {$vals} FROM '{$table}'");
+            return $this->db->query("SELECT {$vals} FROM '{$table}'");
         }
-
-        $this->close();
-        return $result;
     }
 
     public function update($table, $map) {
-        $this->connect();
         $expr = $this->keyValEq($map);
-        $result = $this->db->query("UPDATE '{$table}' SET {$expr};");
-        $this->close();
+        return $this->db->query("UPDATE '{$table}' SET {$expr};");
     }
     
     public function insert($table, $map) {
-        $this->connect();
         $keys = "";
         $vals = "";
         foreach ($map as $key => $value) {
@@ -79,7 +56,31 @@ class AtomicDatabase implements Database {
             $keys = $keys . "'".$key."'";
             $vals = $vals . "'".$value."'";
         }
-        $result = $this->db->query("INSERT INTO {$table} ({$keys}) VALUES ({$vals});");
+        return $this->db->query("INSERT INTO {$table} ({$keys}) VALUES ({$vals});");
+    }
+}
+
+class AtomicDatabase extends Database implements Transactions {
+    use Singleton;
+    protected function __contruct() {
+    }
+
+    public function select($table, $values, $where="") {
+        $this->connect();
+        $result = $this->parent->select($table, $values, $where);
+        $this->close();
+        return $result;
+    }
+
+    public function update($table, $map) {
+        $this->connect();
+        $result = $this->parent->update($table, $map);
+        $this->close();
+    }
+    
+    public function insert($table, $map) {
+        $this->connect();
+        $result = $this->parent->insert($table, $map);
         $this->close();
     }
 }
