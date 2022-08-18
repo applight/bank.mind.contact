127a37b3ac12400493fa7e0232e099d70fed3d8f
Matthew Vaughan <mv@mind.contact>
2022-08-18 19:04:26 -0400

    add logout.php

--- register.php
+++ register.php
@@ -22,7 +22,7 @@ if (    !isset($_POST['firstname']) || !isset($_POST['lastname'])
     $db = AtomicDatabase::getInstance();
 
     $mcdb = new mysqli("127.0.0.1", getenv("DB_USERNAME"), getenv("DB_PASSWORD"), getenv("DB_DATABASE"));
-    $mcdb_result = $mcdb->query("INSERT INTO 'users' ('email','first_name','last_name','phone','password')"
+    $mcdb_result = $mcdb->query("INSERT INTO users (email,first_name,last_name,phone,password)"
                 . " VALUES ('{$email}','{$first}','{$last}','{$phone}','{$password}');"
             );
 
