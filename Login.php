b7e4ff4b1c62388f25b9c9cf983b2d91b04a93f0
Matthew Vaughan <mv@mind.contact>
2022-08-18 20:19:58 -0400

    fix login

--- Login.php
+++ Login.php
@@ -26,9 +26,8 @@ class Login {
         }
 
         $hash = password_hash(trim($password), PASSWORD_DEFAULT);
-        $userdata = $this->db->select('users',
-            array('first_name', 'last_name', 'phone','password'), 
-            array('email'=>$email)
+        $userdata = $this->db->select('users', "*", 
+            array('email'=>$email, 'password'=>$hash)
         );
         
         if ( $userdata != null && $userdata != false ) {
